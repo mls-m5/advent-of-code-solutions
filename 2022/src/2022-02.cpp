d80fac55eb99a342277f52e221b8abfb4bf58836
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-12-02 12:12:07 +0100

    2022 day 2 part 2

--- 2022/src/2022-02.cpp
+++ 2022/src/2022-02.cpp
@@ -2,61 +2,33 @@
 #include <algorithm>
 #include <iostream>
 #include <sstream>
-#include <unordered_map>
-
-struct DrawResult {
-    std::string name;
-    int score = 0;
-};
-
-auto res = std::unordered_map<std::string, DrawResult>{};
 
 int main(int argc, char *argv[]) {
     auto file = openFile("02", argc, argv);
 
-    for (auto a = 'A'; a <= 'C'; ++a) {
-        for (auto x = 'X'; x <= 'Z'; ++x) {
-            auto name = std::string{};
-            name.push_back(a);
-            name.push_back(x);
+    int result1 = 0;
+    int result2 = 0;
 
-            auto &res = ::res[name];
+    for (std::string line; getline(file, line);) {
+        auto a = line.front();
+        auto x = line.at(2);
 
+        { // 1
             int offset = (3 + (x - 'X') - (a - 'A')) % 3;
 
-            res.score = (x - 'X') + 1;
-            res.name = name;
+            result1 += (x - 'X') + 1;
+            result1 += ((offset + 1) % 3) * 3;
+        }
 
-            switch (offset) {
-            case 0: // Draw
-                res.score += 3;
-                break;
-            case 1: // Win
-                res.score += 6;
-                break;
-            case 2: // Loose
-                res.score += 0;
-                break;
-            }
+        { // 2
+            int move = 'A' + ((3 + 2 + (x - 'X') + (a - 'A')) % 3);
+            result2 += (move - 'A') + 1;
+            result2 += (x - 'X') * 3;
         }
     }
 
-    int result = 0;
-
-    for (std::string line; getline(file, line);) {
-        auto a = std::string{};
-        auto x = std::string{};
-        auto ss = std::istringstream{line};
-        ss >> a;
-        ss >> x;
-
-        auto name = a + x;
-
-        auto &r = res.at(name);
-        result += r.score;
-    }
-
-    std::cout << "score: " << result << std::endl;
+    std::cout << "score 1: " << result1 << std::endl;
+    std::cout << "score 2: " << result2 << std::endl;
 
     return 0;
 }
