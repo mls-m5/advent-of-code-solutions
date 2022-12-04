a026fdfb0e8c9c3bf3981c90ae1bec6b1320334c
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-12-04 11:37:01 +0100

    2022 day 4 part 2

--- 2022/src/2022-04.cpp
+++ 2022/src/2022-04.cpp
@@ -3,10 +3,16 @@
 #include <regex>
 #include <sstream>
 
-using P = std::pair<int, int>;
+using Pair = std::pair<int, int>;
 
-bool isContained(P a, P b) {
+// Is a contained in b?
+bool isContained(Pair a, Pair b) {
     return a.first >= b.first && a.second <= b.second;
+}
+
+bool doesOverlap(Pair a, Pair b) {
+    return isContained({a.first, a.first}, b) ||
+           isContained({a.second, a.second}, b) || isContained(b, a);
 }
 
 int main(int argc, char *argv[]) {
@@ -15,29 +21,29 @@ int main(int argc, char *argv[]) {
     auto re = std::regex{"([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)"};
 
     int result1 = 0;
+    int result2 = 0;
 
     for (std::string line; getline(file, line);) {
         auto matches = std::smatch{};
 
         std::regex_match(line, matches, re);
 
-        //        std::cout << matches[1].str() << ", " << matches[2].str() <<
-        //        ", "
-        //                  << matches[3].str() << ", " << matches[4].str() <<
-        //                  std::endl;
-
-        P left{std::stoi(matches[1].str()), std::stoi(matches[2].str())};
-        P right{std::stoi(matches[3].str()), std::stoi(matches[4].str())};
+        Pair left{std::stoi(matches[1].str()), std::stoi(matches[2].str())};
+        Pair right{std::stoi(matches[3].str()), std::stoi(matches[4].str())};
 
         if (isContained(left, right) || isContained(right, left)) {
-            //            std::cout << "is contained in the other\n";
             ++result1;
+        }
+
+        if (doesOverlap(left, right)) {
+            ++result2;
         }
     }
 
     std::cout << "\n";
 
     std::cout << "result 1: " << result1 << std::endl;
+    std::cout << "result 2: " << result2 << std::endl;
 
     return 0;
 }
