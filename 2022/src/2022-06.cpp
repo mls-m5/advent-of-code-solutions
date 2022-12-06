4e63d45d4713fe7c166114ca5263ee5d2fcdbef6
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-12-06 11:16:26 +0100

    2022 day 6 part 2

--- 2022/src/2022-06.cpp
+++ 2022/src/2022-06.cpp
@@ -1,21 +1,23 @@
-
 #include "input.h"
-#include <algorithm>
 #include <iostream>
 #include <set>
 
 int main(int argc, char *argv[]) {
     auto file = openFile("06", argc, argv);
 
-    for (std::string line; getline(file, line);) {
-        for (size_t i = 4; i < line.size(); ++i) {
-            auto set = std::set<char>{};
-            auto markerTest = line.substr(i - 4, 4);
-            set.insert(markerTest.begin(), markerTest.end());
-            if (set.size() == 4) {
-                std::cout << "found start sequence at " << i << "\n";
-                break;
+    auto findSequence = [](auto &line, int markerSize) {
+        for (size_t i = markerSize; i < line.size(); ++i) {
+            auto set =
+                std::set<char>{line.begin() + i - markerSize, line.begin() + i};
+            if (set.size() == markerSize) {
+                return i;
             }
         }
+        return size_t{};
+    };
+
+    for (std::string line; getline(file, line);) {
+        std::cout << "start sequence at " << findSequence(line, 4);
+        std::cout << ", message at " << findSequence(line, 14) << "\n";
     }
 }
--- 2022/src/input.h
+++ 2022/src/input.h
@@ -7,10 +7,7 @@
 
 std::ifstream openFile(std::string name, int argc, char *argv[]) {
     using namespace std::literals;
-
-    auto args = std::vector<std::string_view>(argv + 1, argv + argc);
-    auto isTest = (!args.empty() && args.at(0) == "--test");
-
+    auto isTest = (argc > 1 && argv[1] == "--test"s);
     auto path = std::filesystem::path{"data/"s + name +
                                       (isTest ? "-test.txt" : ".txt")};
 
