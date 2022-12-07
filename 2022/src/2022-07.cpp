6531ff218d2a7060696fe7bce68a0cf55ed7185b
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-12-07 09:39:40 +0100

    2022 day 7 part 2

--- 2022/src/2022-07.cpp
+++ 2022/src/2022-07.cpp
@@ -1,5 +1,4 @@
 #include "input.h"
-#include <algorithm>
 #include <filesystem>
 #include <iostream>
 #include <regex>
@@ -8,17 +7,17 @@
 int main(int argc, char *argv[]) {
     auto file = openFile("07", argc, argv);
 
-    auto current = std::filesystem::path{"/"};
-
     auto cdRe = std::regex{"\\$ cd (.*)"};
     auto fileRe = std::regex{"([0-9]+) (.*)"};
 
     auto dirSizes = std::unordered_map<std::string, int>{};
+    auto current = std::filesystem::path{"/"};
 
     auto countSize = [&dirSizes](std::filesystem::path path, int size) {
         for (; path != "/"; path = path.parent_path()) {
             dirSizes[path.string()] += size;
         }
+        dirSizes[path.string()] += size; // count "/"
     };
 
     for (std::string line; getline(file, line);) {
@@ -27,29 +26,33 @@ int main(int argc, char *argv[]) {
             current = "/"; // First line only
         }
         else if (line == "$ cd ..") {
-            std::cout << "cd parent\n";
             current = current.parent_path();
         }
         else if (std::regex_match(line, match, cdRe)) {
-            std::cout << "cd into " << match[1] << "\n";
             current = current / match[1].str();
         }
         else if (std::regex_match(line, match, fileRe)) {
-            std::cout << "counting file " << match[2] << "\n";
             countSize(current, std::stoi(match[1].str()));
         }
-
-        std::cout << "current: " << current << "\n";
     }
 
     int result1 = 0;
+
+    size_t storageSize = 70000000;
+    auto requiredSpace = 30000000 - (storageSize - dirSizes["/"]);
+    auto smallestSize = storageSize;
 
     for (auto &it : dirSizes) {
         std::cout << "dir " << it.first << ": " << it.second << "\n";
         if (it.second < 100000) {
             result1 += it.second;
         }
+
+        if (it.second > requiredSpace && it.second < smallestSize) {
+            smallestSize = it.second;
+        }
     }
 
     std::cout << "result 1: " << result1 << "\n";
+    std::cout << "result 2: " << smallestSize << "\n";
 }
