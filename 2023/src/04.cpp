b2899da39463f1a06e9317a95e68f9e4950c72d4
Mattias Lasersköld <mattias.l.sk@gmail.com>
2023-12-04 13:19:21 +0100

    2023 day 4 part 2

--- 2023/src/04.cpp
+++ 2023/src/04.cpp
@@ -1,14 +1,22 @@
 
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <set>
 #include <sstream>
 #include <string>
 
-int processLine(std::string line) {
+struct CardResult {
+    int uselessPoints = 0;
+    int index = 0;
+    int newCopies = 0;
+};
+
+CardResult processLine(std::string line) {
     auto ss = std::istringstream{line};
-    std::string trash;
-    ss >> trash >> trash;
+    std::string trash, indexStr;
+    ss >> trash >> indexStr;
+    int index = std::stoi(indexStr);
 
     auto winning = std::set<int>{};
 
@@ -21,6 +29,7 @@ int processLine(std::string line) {
     }
 
     int sum = 0;
+    int sum2 = 0;
 
     for (std::string n; ss >> n;) {
         if (winning.contains(std::stoi(n))) {
@@ -30,10 +39,15 @@ int processLine(std::string line) {
             else {
                 sum <<= 1;
             }
+            sum2 += 1;
         }
     }
 
-    return sum;
+    return {
+        .uselessPoints = sum,
+        .index = index,
+        .newCopies = sum2,
+    };
 }
 
 int main(int argc, char *argv[]) {
@@ -42,9 +56,35 @@ int main(int argc, char *argv[]) {
 
     int sum1 = 0;
 
+    auto resultingCopies = std::map<int, int>{};
+
+    int lastIndex = 0;
+
     for (std::string line; std::getline(file, line);) {
-        sum1 += processLine(line);
+        auto res = processLine(line);
+        sum1 += res.uselessPoints;
+
+        auto &numCopies = resultingCopies[res.index];
+        // First copy
+        ++numCopies;
+
+        for (int i = res.index + 1; i <= res.index + res.newCopies; ++i) {
+            resultingCopies[i] += numCopies;
+        }
+
+        lastIndex = res.index;
+    }
+
+    auto it = resultingCopies.find(lastIndex);
+    ++it;
+    resultingCopies.erase(it, resultingCopies.end());
+
+    int sum2 = 0;
+    for (auto it : resultingCopies) {
+        std::cout << it.first << ": " << it.second << "\n";
+        sum2 += it.second;
     }
 
     std::cout << "sum 1: " << sum1 << std::endl;
+    std::cout << "sum 2: " << sum2 << std::endl;
 }
