afda4fadb2c013d676b1b850b42a60e3dc6eb704
Mattias Lasersköld <mattias.l.sk@gmail.com>
2023-12-12 09:33:40 +0100

    2023 day 8 part 2 - unfinished

--- 2023/src/08.cpp
+++ 2023/src/08.cpp
@@ -1,8 +1,11 @@
+#include <algorithm>
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <ostream>
 #include <regex>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 
 struct Element {
@@ -14,12 +17,12 @@ struct Element {
 };
 
 int main(int argc, char *argv[]) {
-    auto start = std::chrono::high_resolution_clock::now();
 
     auto file = std::ifstream{std::string{"data/08"} +
                               (argc <= 1 ? ".txt" : "-test.txt")};
 
-    auto elements = std::unordered_map<std::string, Element>{};
+    // auto elements = std::unordered_map<std::string, Element>{};
+    auto elements = std::vector<Element>{};
     auto current = std::vector<Element *>{};
 
     auto sequence = std::string{};
@@ -28,7 +31,8 @@ int main(int argc, char *argv[]) {
 
     auto match = std::smatch{};
 
-    auto re = std::regex{R"_(([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\))_"};
+    auto re =
+        std::regex{R"_(([A-Z0-9]{3}) = \(([A-Z0-9]{3}), ([A-Z0-9]{3})\))_"};
 
     for (std::string line; std::getline(file, line);) {
         if (line.empty()) {
@@ -44,40 +48,117 @@ int main(int argc, char *argv[]) {
             std::cout << match[1] << " = " << match[2] << "," << match[3]
                       << "\n";
 
-            elements[name] = Element{name, left, right};
+            // elements[name] = Element{name, left, right};
+            elements.push_back({name, left, right});
         }
     }
 
+    auto at = [&](std::string name) {
+        auto f = std::find_if(
+            elements.begin(), elements.end(), [name](auto &element) {
+                return element.name == name;
+            });
+
+        if (f == elements.end()) {
+            throw std::out_of_range{"could not find element"};
+        }
+        return &*f;
+    };
+
     for (auto &element : elements) {
-        element.second.left = &elements.at(element.second.leftName);
-        element.second.right = &elements.at(element.second.rightName);
+        // element.second.left = &elements.at(element.second.leftName);
+        // element.second.right = &elements.at(element.second.rightName);
+
+        element.left = at(element.leftName);
+        element.right = at(element.rightName);
+
+        if (element.name.ends_with('A')) {
+            current.push_back(&element);
+        }
     }
 
-    long long steps = 0;
+    try {
+        auto start = std::chrono::high_resolution_clock::now();
+        long long steps = 0;
+        size_t sequenceIndex = 0;
+        for (auto element = at("AAA");; ++steps) {
+            if (element->name == "ZZZ") {
+                break;
+            }
+
+            if (sequence.at(sequenceIndex) == 'L') {
+                element = element->left;
+            }
+            else {
+                element = element->right;
+            }
+
+            sequenceIndex += 1;
+            if (sequenceIndex >= sequence.size()) {
+                sequenceIndex = 0;
+            }
+        }
+
+        std::cout << steps << std::endl;
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto duration = stop - start;
+        std::cout << "us time: "
+                  << std::chrono::duration_cast<std::chrono::microseconds>(
+                         duration)
+                         .count()
+                  << std::endl;
+    }
+    catch (...) {
+        std::cout << "probably not the first type" << std::endl;
+    }
+
+    long long steps = 1;
     size_t sequenceIndex = 0;
-    for (auto element = &elements.at("AAA");; ++steps) {
-        if (element->name == "ZZZ") {
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (;;) {
+
+        if (elements.size() < 20) {
+            for (auto &c : current) {
+                std::cout << c->name << " ";
+            }
+            std::cout << "\n";
+        }
+
+        bool isDone = true;
+        for (auto &c : current) {
+            auto &element = c;
+            if (sequence.at(sequenceIndex) == 'L') {
+                element = element->left;
+            }
+            else {
+                element = element->right;
+            }
+
+            if (c->name.back() != 'Z') {
+                isDone = false;
+            }
+        }
+
+        if (isDone) {
             break;
         }
 
-        if (sequence.at(sequenceIndex) == 'L') {
-            element = element->left;
-        }
-        else {
-            element = element->right;
-        }
+        ++steps;
 
         sequenceIndex += 1;
         if (sequenceIndex >= sequence.size()) {
             sequenceIndex = 0;
         }
+
+        if (steps % 10000000 == 0) {
+            std::cout << "currently " << steps << " steps" << std::endl;
+        }
     }
 
-    std::cout << steps << std::endl;
-
+    std::cout << "ghost steps: " << steps << std::endl;
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = stop - start;
-
     std::cout << "us time: "
               << std::chrono::duration_cast<std::chrono::microseconds>(duration)
                      .count()
