faec71a56f90e0d02ac3f2d593faab392aead717
Mattias Lasersköld <mattias.l.sk@gmail.com>
2024-12-06 11:12:58 +0100

    2024 day 2 part 1

--- 2024/src/02.cpp
+++ 2024/src/02.cpp
@@ -1,12 +1,51 @@
+#include <cmath>
+#include <format>
 #include <fstream>
 #include <iostream>
+#include <optional>
+#include <sstream>
 #include <string>
 
 int main(int argc, char *argv[]) {
     auto file = std::ifstream{std::string{"data/02"} +
                               (argc <= 1 ? ".txt" : "-test.txt")};
 
+    int numSafe = 0;
+
     for (std::string line; std::getline(file, line);) {
-        std::cout << line << std::endl;
+        auto ss = std::istringstream{line};
+
+        bool isSafe = true;
+        auto prev = std::optional<int>{};
+        int dir = 0;
+
+        for (int i = 0; ss >> i; prev = i) {
+            if (!prev) {
+                continue;
+            }
+
+            auto d = *prev - i;
+            auto dist = std::abs(d);
+            auto tmpDir = std::copysign(1, d);
+
+            if (dist < 1 || dist > 3) {
+                isSafe = false;
+                break;
+            }
+
+            if (dir == 0) {
+                dir = tmpDir;
+            }
+
+            if (dir != tmpDir) {
+                isSafe = false;
+            }
+
+            dir = tmpDir;
+        }
+
+        numSafe += isSafe;
     }
+
+    std::cout << std::format("Part 1: {}\n", numSafe);
 }
