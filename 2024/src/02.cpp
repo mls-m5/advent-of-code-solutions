d396095ec92fda845ac68123d51264f02bb98735
Mattias Lasersköld <mattias.l.sk@gmail.com>
2024-12-06 11:27:31 +0100

    2024 day 2 part 2

--- 2024/src/02.cpp
+++ 2024/src/02.cpp
@@ -5,26 +5,42 @@
 #include <optional>
 #include <sstream>
 #include <string>
+#include <vector>
 
 int main(int argc, char *argv[]) {
     auto file = std::ifstream{std::string{"data/02"} +
                               (argc <= 1 ? ".txt" : "-test.txt")};
 
-    int numSafe = 0;
+    int numSafe1 = 0;
+    int numSafe2 = 0;
+
+    auto numbers = std::vector<std::vector<int>>{};
 
     for (std::string line; std::getline(file, line);) {
         auto ss = std::istringstream{line};
 
+        numbers.emplace_back();
+        for (int i = 0; ss >> i;) {
+            numbers.back().push_back(i);
+        }
+    }
+
+    auto isSafeF = [](auto &nums, int ignore = 0) {
         bool isSafe = true;
         auto prev = std::optional<int>{};
         int dir = 0;
 
-        for (int i = 0; ss >> i; prev = i) {
+        for (int i = 0; i < nums.size(); ++i) {
+            if (ignore == i) {
+                continue;
+            }
+            auto value = nums.at(i);
             if (!prev) {
+                prev = value;
                 continue;
             }
 
-            auto d = *prev - i;
+            auto d = *prev - value;
             auto dist = std::abs(d);
             auto tmpDir = std::copysign(1, d);
 
@@ -42,10 +58,30 @@ int main(int argc, char *argv[]) {
             }
 
             dir = tmpDir;
+            prev = value;
+        }
+        return isSafe;
+    };
+
+    auto toleratingIsSafe = [&isSafeF](auto &nums) {
+        if (isSafeF(nums)) {
+            return true;
         }
 
-        numSafe += isSafe;
+        for (size_t i = 0; i < nums.size(); ++i) {
+            if (isSafeF(nums, i)) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    for (auto &nums : numbers) {
+        numSafe1 += isSafeF(nums);
+        numSafe2 += toleratingIsSafe(nums);
     }
 
-    std::cout << std::format("Part 1: {}\n", numSafe);
+    std::cout << std::format("Part 1: {}\n", numSafe1);
+    std::cout << std::format("Part 2: {}\n", numSafe2);
 }
