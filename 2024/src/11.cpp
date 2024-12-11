1e567ffeb7edac512fcf8a1619c02880767bcebd
Mattias Lasersköld <mattias.l.sk@gmail.com>
2024-12-11 11:40:36 +0100

    2024 day 11 part 2

--- 2024/src/11.cpp
+++ 2024/src/11.cpp
@@ -36,37 +36,45 @@ int main(int argc, char *argv[]) {
         ++numbers[num];
     }
 
-    auto next = decltype(numbers){};
+    auto count = [](int iterations, auto numbers) {
+        auto next = decltype(numbers){};
+        for (int i = 0; i < iterations; ++i) {
+            for (auto &it : numbers) {
+                if (it.first == 0) {
+                    next[1] += it.second;
+                }
+                else if (auto digits = numberOfDigits(it.first);
+                         digits % 2 == 0) {
+                    auto multiplier = getMultiplier(digits / 2 + 1);
+                    auto a = it.first / multiplier;
+                    auto b = it.first % multiplier;
+                    next[a] += it.second;
+                    next[b] += it.second;
+                }
+                else {
+                    next[it.first * 2024] += it.second;
+                }
+            }
+            std::swap(numbers, next);
+            next.clear();
 
-    for (int i = 0; i < 25; ++i) {
-        for (auto &it : numbers) {
-            if (it.first == 0) {
-                next[1] += it.second;
-            }
-            else if (auto digits = numberOfDigits(it.first); digits % 2 == 0) {
-                auto multiplier = getMultiplier(digits / 2 + 1);
-                auto a = it.first / multiplier;
-                auto b = it.first % multiplier;
-                next[a] += it.second;
-                next[b] += it.second;
-            }
-            else {
-                next[it.first * 2024] += it.second;
-            }
+            // for (auto &it : numbers) {
+            //     std::cout << std::format("{}={} ", it.first, it.second);
+            // }
+            // std::cout << "\n";
         }
-        std::swap(numbers, next);
-        next.clear();
+        auto sum1 = std::accumulate(
+            numbers.begin(),
+            numbers.end(),
+            uint64_t{},
+            [](uint64_t sum, auto &b) { return sum + b.second; });
 
-        // for (auto &it : numbers) {
-        //     std::cout << std::format("{}={} ", it.first, it.second);
-        // }
-        // std::cout << "\n";
-    }
+        return sum1;
+    };
 
-    auto sum1 = std::accumulate(
-        numbers.begin(), numbers.end(), uint64_t{}, [](uint64_t sum, auto &b) {
-            return sum + b.second;
-        });
+    auto sum1 = count(25, numbers);
+    auto sum2 = count(75, numbers);
 
     std::cout << std::format("Part 1: {}\n", sum1);
+    std::cout << std::format("Part 2: {}\n", sum2);
 }
