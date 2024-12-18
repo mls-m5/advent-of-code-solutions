57f38aec5f3ea011bc7ac508481e5e4cff7916da
Mattias Lasersköld <mattias.l.sk@gmail.com>
2024-12-18 10:31:24 +0100

    2024 day 17 part 2

--- 2024/src/17.cpp
+++ 2024/src/17.cpp
@@ -5,13 +5,15 @@
 #include <iostream>
 #include <istream>
 #include <ranges>
+#include <ratio>
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <thread>
 #include <vector>
 
 using std::ranges::iota_view;
-using Int = int;
+using Int = long;
 struct Cpu;
 
 using OpFT = void (*)(Cpu &);
@@ -26,7 +28,17 @@ struct Cpu {
     Int pc = 0;
 
     std::vector<char> program;
-    std::string result;
+    std::vector<char> outputValues;
+    std::vector<int> usedOperations;
+
+    void clear() {
+        a = 0;
+        b = 0;
+        c = 0;
+        pc = 0;
+        outputValues.clear();
+        usedOperations.clear();
+    }
 
     static Int readRegister(std::istream &stream) {
         std::string line;
@@ -85,23 +97,48 @@ Result: {}
                                  b,
                                  c,
                                  pc,
-                                 result);
+                                 resultString());
     }
 
     void printProgram() const {
+        std::cout << "program:";
         for (auto p : program) {
-            std::cout << static_cast<int>(p) << ", ";
+            std::cout << static_cast<int>(p) << ",";
         }
         std::cout << "\n";
     }
 
-    void output(Int value) {
-        // std::cout << value << ",";
-        result += std::to_string(value) + ",";
+    void printUsedOperations() const {
+        for (auto o : usedOperations) {
+            std::cout << o << " ";
+        }
+        std::cout << std::endl;
     }
 
-    void step() {
+    void output(Int value) {
+        outputValues.push_back(value);
+    }
+
+    std::string resultString() const {
+        auto ret = std::string{};
+
+        if (outputValues.empty()) {
+            return ret;
+        }
+
+        for (auto o : outputValues) {
+            ret += std::to_string(static_cast<int>(o)) + ",";
+        }
+        ret.pop_back();
+        return ret;
+    }
+
+    void step(bool trace) {
         auto instruction = program.at(pc);
+
+        if (trace) {
+            usedOperations.push_back(instruction);
+        }
 
         switch (instruction) {
         case 0: // adv
@@ -137,11 +174,19 @@ Result: {}
         pc += 2;
     }
 
-    void run(bool print) {
+    void run(bool print,
+             bool haltBasedOnOutputValue = false,
+             bool trace = false) {
         for (; pc < program.size();) {
-            step();
+            step(trace);
             if (print) {
                 printState();
+            }
+
+            if (haltBasedOnOutputValue) {
+                if (outputValues.size() > program.size()) {
+                    break;
+                }
             }
         }
     }
@@ -170,7 +215,7 @@ Register C: 0
 
     auto cpu = Cpu{ss};
     cpu.run(false);
-    assert(cpu.result == "0,1,2,");
+    assert(cpu.resultString() == "0,1,2");
 }
 
 void test3() {
@@ -183,7 +228,7 @@ Register C: 0
 
     auto cpu = Cpu{ss};
     cpu.run(false);
-    assert(cpu.result == "4,2,5,6,7,7,7,7,3,1,0,");
+    assert(cpu.resultString() == "4,2,5,6,7,7,7,7,3,1,0");
     assert(cpu.a == 0);
 }
 
@@ -213,6 +258,21 @@ Register C: 43690
     assert(cpu.b == 44354);
 }
 
+void test6() {
+    auto ss = std::istringstream{R"_(Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0
+)_"};
+
+    auto cpu = Cpu{ss};
+    cpu.a = 117440;
+    cpu.run(false);
+    cpu.printState();
+    assert(cpu.program == cpu.outputValues);
+}
+
 int main(int argc, char *argv[]) {
     auto file = std::ifstream{std::string{"data/17"} +
                               (argc <= 1 ? ".txt" : "-test.txt")};
@@ -222,20 +282,59 @@ int main(int argc, char *argv[]) {
     test3();
     test4();
     test5();
+    test6();
 
     auto cpu = Cpu{file};
+    auto cpuCopy = cpu;
     cpu.printState();
     cpu.printProgram();
 
     cpu.run(argc > 1);
 
     cpu.printState();
-    std::cout << cpu.result << std::endl;
+    std::cout << cpu.resultString() << std::endl;
 
-    for (size_t i = 0; i < cpu.result.size(); i += 2) {
-        std::cout << cpu.result.at(i);
+    {
+        auto result = cpu.resultString();
+        for (size_t i = 0; i < result.size(); i += 2) {
+            std::cout << result.at(i);
+        }
+        std::cout << std::endl;
+
+        cpu.printUsedOperations();
     }
-    std::cout << std::endl;
+
+    {
+        auto program = cpu.program;
+
+        std::cout << "Trying to find the right value of the cpus register\n";
+        auto testCpu = cpuCopy;
+        Int start = 40'000'000'000'000;
+        // Int start = 1000;
+        for (auto i : iota_view(start, start + 1000'000'000)) {
+            testCpu.clear();
+            testCpu.a = i;
+
+            testCpu.run(false, true);
+            if (testCpu.outputValues == testCpu.program) {
+                std::cout << std::format("found value: {}\n", i);
+                break;
+            }
+            // if (i % 1'000'000 == 0) {
+            if (i % 1'00 == 0) {
+                testCpu.clear();
+                testCpu.a = i;
+                testCpu.run(false, true, true);
+                std::cout << i << std::endl;
+                testCpu.printState();
+                testCpu.printProgram();
+                testCpu.printUsedOperations();
+                std::cout.flush();
+                using namespace std::chrono_literals;
+                std::this_thread::sleep_for(400ms);
+            }
+        }
+    }
 
     // 367057314  is not right
 }
